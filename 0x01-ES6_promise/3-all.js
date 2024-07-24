a177254f6069a65eee7775521a29b34e63eede4a
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-07-25 02:18:46 +0300

    ss

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,5 @@
-import {uploadPhoto, createUser} from "./utils.js";
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-	return Promise
-	.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()]);
 }
