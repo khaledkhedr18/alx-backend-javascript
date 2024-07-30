cafb467faef0c510e54ddb2c425ddeec682fcbfc
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-07-31 02:48:23 +0300

    4th task

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,21 +5,16 @@
  * if a student doesnt have grade in newGrades, the final grade should be N/A
  * you must use filter and map combined
  */
-
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const filteredStudents = students.filter(
-    (student) => student.location === city,
-  );
-  const mappedStudents = filteredStudents.map((student) => {
-    for (const gradeinfo of newGrades) {
-      if (student.id === gradeinfo.studentId) {
-        student.grade = newGrades.grade; // eslint-disable-line no-param-reassign
-      }
-    }
-    if (student.grade === undefined) {
-      student.grade = 'N/A'; // eslint-disable-line no-param-reassign
-    }
-    return student;
-  });
-  return mappedStudents;
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeInfo = newGrades.find(
+        (grade) => grade.studentId === student.id
+      );
+      return {
+        ...student,
+        grade: gradeInfo ? gradeInfo.grade : 'N/A',
+      };
+    });
 }
