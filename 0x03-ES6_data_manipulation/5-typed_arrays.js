ee0fc0d050482890b4da8310693d729da45c68b3
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-07-31 02:09:08 +0300

    ss

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
-    throw new Error('Position out of range');
+    throw new Error('Position outside range');
   }
   const dv = new DataView(new ArrayBuffer(length), 0, length);
   dv.setUint8(position, value);
