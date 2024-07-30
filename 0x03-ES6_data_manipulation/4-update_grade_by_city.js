81261f2e6e8e91522dbf12ec16033314a5aac4a1
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-07-31 02:49:42 +0300

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -10,7 +10,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
     .filter((student) => student.location === city)
     .map((student) => {
       const gradeInfo = newGrades.find(
-        (grade) => grade.studentId === student.id
+        (grade) => grade.studentId === student.id,
       );
       return {
         ...student,
